a05fbbce97fa3a34cce7415761e5dbec60d3fc05
grossmanlev <grossmanlev>
2017-11-28 23:46:25 -0500

    q-learning, working...

--- dev/lev-test.py
+++ dev/lev-test.py
@@ -24,34 +24,37 @@ from hfo import *
 
 import random
 import math
+import numpy as np
 
 STATES = 100*100
 ACTIONS = 4
 
-TEAMMATES = 2
+TEAMMATES = 1
 OPPONENTS = 2
 
 EPSILON = 0.05
-APLHA = 0.25
+ALPHA = 0.1
 GAMMA = 0.9
 
 # Gets tile in range 0-99 from (x,y) position
 def getTile(x, y):
-  x = math.floor(10*(x+1))
-  y = math.floor(10*(y+1))
+  x = int(math.floor(10*(x+1)))
+  y = int(math.floor(10*(y+1)))
 
-  if x == 10:
+  if x > 9:
     x = 9
-  if y == 10:
+  if y > 9:
     y = 9
   
+
+  #print(10*y + x)
   return 10*y + x
 
 # returns if an opponent is in same tile as ball (they probably have the ball)
 def oppHasBall(state):
   ball_tile = getTile(state[3], state[4])
   for o in range(OPPONENTS):
-    o_x,o_y = (state[10+6*TEAMMATES+(3*o)],  state[10+6*TEAMMATES+(3*o)+1])
+    o_x,o_y = (state[9+6*TEAMMATES+(3*o)],  state[9+6*TEAMMATES+(3*o)+1])
     opp_tile = getTile(o_x, o_y)
     if ball_tile == opp_tile:
       return True
@@ -78,14 +81,15 @@ def main():
       for k in range(ACTIONS):
         qvals[i][j][k] = random.random()
 
-
-
+  episode_num = 0
   for episode in itertools.count():
+    episode_num += 1
     status = IN_GAME
 
    # state = hfo.getState()
 
     state = hfo.getState()
+    print(len(state))
     robot_tile = getTile(state[0], state[1])
     ball_tile = getTile(state[3], state[4])
 
@@ -110,6 +114,7 @@ def main():
 
       # Grab the state features from the environment
       state = hfo.getState()
+      #print(len(state)) 23?!
       next_robot_tile = getTile(state[0], state[1])
       next_ball_tile = getTile(state[3], state[4])
 
@@ -118,16 +123,15 @@ def main():
       #TODO: get the reward!
       r = 0
       if status == GOAL:
-        r = -20
-      elif status == CAPTURED_BY_DEFENSE or status == OUT_OF_BOUNDS:
-        r = 15
-      elif oppHasBall(state):
         r = -5
+      elif status == CAPTURED_BY_DEFENSE or status == OUT_OF_BOUNDS:
+        r = 10
+      elif oppHasBall(state):
+        r = -1
       else:
         r = 0
 
-      qvals[robot_tile][ball_tile][a] +=
-        ALPHA*(r + GAMMA*max(qvals[next_robot_tile][next_ball_tile]) - qvals[robot_tile][ball_tile])
+      qvals[robot_tile][ball_tile][a] += ALPHA*(r + (GAMMA*max(qvals[next_robot_tile][next_ball_tile])) - qvals[robot_tile][ball_tile][a])
 
       robot_tile = next_robot_tile
       ball_tile = next_ball_tile
@@ -154,9 +158,14 @@ def main():
     # Check the outcome of the episode
     print(('Episode %d ended with %s'%(episode, hfo.statusToString(status))))
     # Quit if the server goes down
+    if episode_num % 5 == 0:
+      q = np.array(qvals)
+      np.save('q.npy', q)
+
     if status == SERVER_DOWN:
       hfo.act(QUIT)
       exit()
+
 
 
 
