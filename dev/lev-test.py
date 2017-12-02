9057521edd79932e8d9a64bfe87197dca8e239b6
grossmanlev <grossmanlev>
2017-12-02 12:59:02 -0500

    tile change, important

--- dev/lev-test.py
+++ dev/lev-test.py
@@ -26,18 +26,18 @@ import random
 import math
 import numpy as np
 
-STATES = 100*100
-ACTIONS = 4
+STATES = 6*100*100
+ACTIONS = 6
 
 TEAMMATES = 1
 OPPONENTS = 2
 
-EPSILON = 0.05
-ALPHA = 0.2
-GAMMA = 0.9
+EPSILON = 0 #0.025
+ALPHA = 0.125
+GAMMA = 0.99
 
 TRAIN = False
-RANDOM = False
+RANDOM = True
 
 # Gets tile in range 0-99 from (x,y) position
 def getTile(x, y):
@@ -52,6 +52,20 @@ def getTile(x, y):
 
   #print(10*y + x)
   return 10*y + x
+
+# Gets tile in range 0-5 from y position of goalie
+def getGoalieTile(y):
+  if y < -0.2:
+    return 0
+  if y < -0.1:
+    return 1
+  if y < 0:
+    return 2
+  if y < 0.1:
+    return 3
+  if y < 0.2:
+    return 4
+  return 5
 
 # returns if an opponent is in same tile as ball (they probably have the ball)
 def oppHasBall(state):
@@ -79,11 +93,12 @@ def main():
   #   qvals[i] = random.random()
 
   if TRAIN:
-    qvals = [[[0 for k in range(ACTIONS)] for j in range(100)] for i in range(100)]
-    for i in range(100):
-      for j in range(100):
-        for k in range(ACTIONS):
-          qvals[i][j][k] = random.random()
+    qvals = [[[[0 for k in range(ACTIONS)] for j in range(100)] for i in range(100)] for g in range(6)]
+    for g in range(6):
+      for i in range(100):
+        for j in range(100):
+          for k in range(ACTIONS):
+            qvals[g][i][j][k] = random.random()
   else:
     qvals = np.load('q.npy').tolist()
 
@@ -92,17 +107,18 @@ def main():
     episode_num += 1
     status = IN_GAME
 
-   # state = hfo.getState()
-
+    # state = hfo.getState()
+    # TODO add goalie location too?
     state = hfo.getState()
     robot_tile = getTile(state[0], state[1])
     ball_tile = getTile(state[3], state[4])
+    goalie_tile = getGoalieTile(state[9+3*TEAMMATES+1])
 
     while status == IN_GAME:
 
       if not RANDOM:
         # Pick new action, a', to take with epsilon-greedy strategy
-        a = qvals[robot_tile][ball_tile].index(max(qvals[robot_tile][ball_tile]))
+        a = qvals[goalie_tile][robot_tile][ball_tile].index(max(qvals[goalie_tile][robot_tile][ball_tile]))
         if random.random() < EPSILON:
           a = random.randint(0, ACTIONS-1)
       else:
@@ -114,6 +130,10 @@ def main():
         hfo.act(REDUCE_ANGLE_TO_GOAL)
       elif a == 2:
         hfo.act(DEFEND_GOAL)
+      elif a == 3:
+        hfo.act(NOOP)
+      elif a == 4:
+        hfo.act(MOVE)
       else:
         hfo.act(GO_TO_BALL)
 
@@ -125,25 +145,27 @@ def main():
       #print(len(state)) 23?!
       next_robot_tile = getTile(state[0], state[1])
       next_ball_tile = getTile(state[3], state[4])
+      next_goalie_tile = getGoalieTile(state[10+3*TEAMMATES+1])
 
       # Get reward, update Q-val
 
       #TODO: get the reward!
       r = 0
       if status == GOAL:
-        r = -7.5
+        r = -1
       elif status == CAPTURED_BY_DEFENSE or status == OUT_OF_BOUNDS:
-        r = 10
+        r = 1
       #elif oppHasBall(state):
       #  r = -1
       else:
         r = 0
 
       if TRAIN:
-        qvals[robot_tile][ball_tile][a] += ALPHA*(r + (GAMMA*max(qvals[next_robot_tile][next_ball_tile])) - qvals[robot_tile][ball_tile][a])
-
+        qvals[goalie_tile][robot_tile][ball_tile][a] += ALPHA*(r + (GAMMA*max(qvals[next_goalie_tile][next_robot_tile][next_ball_tile])) - qvals[goalie_tile][robot_tile][ball_tile][a])
+      
       robot_tile = next_robot_tile
       ball_tile = next_ball_tile
+      goalie_tile = next_goalie_tile
 
 
       # reward: -5 if enemy has ball, -20 if goal, +15 OOB, CAPTURED_BY_DEFENSE
